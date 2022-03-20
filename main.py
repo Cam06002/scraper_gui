ea634f9b32fe63a7fe6039f1af7513d51f478092
Jeff Campbell <jeffcampbell333@gmail.com>
2022-03-20 01:24:55 -0700

    Update

--- main.py
+++ main.py
@@ -18,9 +18,17 @@ url_box = ttk.Entry(root, width=50)
 url_box.grid(column=1, row=2, padx=15, pady=5)
 
     # listing all labels to search
-ttk.Label(text="List HTML labels to search, separated by a comma:").grid(column=1, row=4, padx=15, pady=10)
+ttk.Label(text="List Top Class levels to search, separated by a comma:").grid(column=1, row=3, padx=15, pady=10)
+top_list = ttk.Entry(root, width=50)
+top_list.grid(column=1, row=4, padx=15, pady=5)
+
+ttk.Label(text="List Secondary Class levels to search, separated by a comma:").grid(column=1, row=5, padx=15, pady=10)
+secondary_list = ttk.Entry(root, width=50)
+secondary_list.grid(column=1, row=6, padx=15, pady=5)
+
+ttk.Label(text="List HTML types to search, separated by a comma:").grid(column=1, row=7, padx=15, pady=10)
 label_list = ttk.Entry(root, width=50)
-label_list.grid(column=1, row=5, padx=15, pady=5)
+label_list.grid(column=1, row=8, padx=15, pady=5)
 
 
 def scout():
@@ -28,21 +36,18 @@ def scout():
     # Main URL sourcing
     url_text = url_box.get()
     data = r.get(url_text)
-    soup = data.text
-    return soup
+    soup = bs(data.content, "html.parser")
 
-
-def add_labels():
-
-    html_labels = str(label_list.get())
+    html_labels = str(top_list.get())
     html_labels_list = html_labels.split(', ')
-    print(html_labels_list)
+
+    for label in html_labels_list:
+        item_finder = soup.find_all("div", class_=label)
+        for content in item_finder:
+            print(content, end="\n"*2)
 
 
 scout_button = ttk.Button(root, text="Scout", command=scout)
-scout_button.grid(column=1, row=3, padx=30, pady=10)
-
-add_labels_button = ttk.Button(root, text="Add Labels", command=add_labels)
-add_labels_button.grid(column=1, row=6, padx=30, pady=10)
+scout_button.grid(column=1, row=5, padx=30, pady=10)
 
 root.mainloop()
